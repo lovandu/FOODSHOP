8d5c5f874bfe400b8b27ac44260b2208c8265f6b
lovandu <lodu4289@gmail.com>
2022-08-07 21:49:19 +0700

    prepare deploy

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
     process.env.NODE_ENV !== 'production'
         ? 'http://localhost:5000/api'
-        : 'https://foodshop-mern-web.herokuapp.com//api';
+        : 'https://radiant-harbor-68164.herokuapp.com/api';
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern';
 
