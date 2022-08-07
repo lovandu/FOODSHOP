3c126bc9898434962321e57f3fc45adc67289909
lovandu <lodu4289@gmail.com>
2022-08-07 20:25:27 +0700

    done

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,11 +1,9 @@
 export const apiUrl =
     process.env.NODE_ENV !== 'production'
         ? 'http://localhost:5000/api'
-        : 'https://sleepy-inlet-56101.herokuapp.com/api';
+        : 'https://foodshop-mern-web.herokuapp.com//api';
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern';
-
-
 
 export const PRODUCT_LOADED_SUCCESS = 'PRODUCT_LOADED_SUCCESS';
 export const PRODUCT_LOADED_FAIL = 'PRODUCT_LOADED_FAIL';
@@ -14,7 +12,6 @@ export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 export const FIND_PRODUCT = 'FIND_PRODUCT';
 export const FILTER_PRODUCT = 'FILTER_PRODUCT';
-
 
 export const CART_LOADED_SUCCESS = 'CART_LOADED_SUCCESS';
 export const CART_LOADED_FAIL = 'CART_LOADED_FAIL';
@@ -28,9 +25,7 @@ export const ORDER_LOADED_SUCCESS = 'ORDER_LOADED_SUCCESS';
 export const ORDER_LOADED_FAIL = 'ORDER_LOADED_FAIL';
 export const ADD_ORDER = 'ADD_ORDER';
 
-
 // convert category
-export const FOOD='Đồ Ăn'
-export const BEVERAGE='Đồ Uống'
-export const DRY='Đồ Khô'
-
+export const FOOD = 'Đồ Ăn';
+export const BEVERAGE = 'Đồ Uống';
+export const DRY = 'Đồ Khô';
